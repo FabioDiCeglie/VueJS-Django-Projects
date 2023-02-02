77efbd927e4e7c57bdc9e5a5b61a360d0b2ca08d
FabioDiCeglie <dcfabio96@gmail.com>
2023-02-02 21:08:38 +0100

    start working on the app

--- Weight-Tracker/src/App.vue
+++ Weight-Tracker/src/App.vue
@@ -1,4 +1,13 @@
-<script setup></script>
+<script setup>
+import { ref, shallowRef, computed, watch, nextTick } from "vue";
+import Chart from "chart.js/auto";
+
+const weight = ref([]);
+
+const weightChartEl = ref(null);
+
+const weightChart = shallowRef(null);
+</script>
 
 <template>
   <h1>Hello</h1>
