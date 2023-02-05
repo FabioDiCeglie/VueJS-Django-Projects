1a20a7e56128a1ccfc6a0b7d624b83a81eecb41d
FabioDiCeglie <dcfabio96@gmail.com>
2023-02-05 11:50:39 +0100

    chore: show first product

--- Django-Vue-E-Commerce/frontend/src/main.js
+++ Django-Vue-E-Commerce/frontend/src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from "vue";
 import router from "./router";
 import App from "./App.vue";
+import axios from "axios";
 
-createApp(App).use(router).mount("#app");
+axios.defaults.baseURL = "http://127.0.0.1:8000";
+
+createApp(App).use(router, axios).mount("#app");
