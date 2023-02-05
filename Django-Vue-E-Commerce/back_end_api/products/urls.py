fc90e0b2541136bd6c2ed43b2f0cc12708cc1b02
FabioDiCeglie <dcfabio96@gmail.com>
2023-02-05 17:02:14 +0100

    feat: created view set for product detail

--- Django-Vue-E-Commerce/back_end_api/products/urls.py
+++ Django-Vue-E-Commerce/back_end_api/products/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path,include
+from django.urls import path
 
-from products import views
+from .views import LatestProductsList, ProductDetail
+
 
 urlpatterns = [
-    path("latest-products/", views.LatestProductsList.as_view())
+    path("latest-products/", LatestProductsList.as_view()),
+    path("products/<slug:category_slug>/<slug:product_slug>/", ProductDetail.as_view())
 ]
--- Django-Vue-E-Commerce/back_end_api/products/views.py
+++ Django-Vue-E-Commerce/back_end_api/products/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.http import Http404
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -10,4 +11,16 @@ class LatestProductsList(APIView):
     def get(self, request, format=None):
         products = Product.objects.all()[0:4]
         serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+class ProductDetail(APIView):
+    def get_object(self, category_slug, product_slug):
+        try:
+            return Product.objects.filter(category_slug=category_slug).get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise Http404
+
+    def get(self, request, category_slug, product_slug, format=None):
+        product = self.get_object(category_slug, product_slug)
+        serializer = ProductSerializer(product)
         return Response(serializer.data)
