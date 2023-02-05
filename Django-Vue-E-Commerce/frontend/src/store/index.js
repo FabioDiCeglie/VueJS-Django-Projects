ebe95b097609ddfc93b14103a43bccb8fa08430d
FabioDiCeglie <dcfabio96@gmail.com>
2023-02-05 17:33:31 +0100

    feat: set up vuex/state

---
+++ Django-Vue-E-Commerce/frontend/src/store/index.js
@@ -0,0 +1,15 @@
+import { createStore } from "vuex";
+
+export default createStore({
+  state: {
+    cart: {
+      items: [],
+    },
+    isAuthenticated: false,
+    token: "",
+    isLoading: false,
+  },
+  mutations: {},
+  actions: {},
+  modules: {},
+});
