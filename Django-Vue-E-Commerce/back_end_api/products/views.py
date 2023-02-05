6fd8b9dc2cae3bab19542d8a6e882932cd75c37b
FabioDiCeglie <dcfabio96@gmail.com>
2023-02-05 11:21:39 +0100

    fix: fixed import view

--- Django-Vue-E-Commerce/back_end_api/products/views.py
+++ Django-Vue-E-Commerce/back_end_api/products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from rest_framework import APIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import Product
