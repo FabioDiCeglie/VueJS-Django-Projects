150d3ffe34bd53a1b8d040179e013f1bd3411494
FabioDiCeglie <dcfabio96@gmail.com>
2023-02-06 21:19:25 +0100

    feat: created search view

--- Django-Vue-E-Commerce/back_end_api/products/urls.py
+++ Django-Vue-E-Commerce/back_end_api/products/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import LatestProductsList, ProductDetail, CategoryDetail
+from .views import LatestProductsList, ProductDetail, CategoryDetail, search
 
 
 urlpatterns = [
     path("latest-products/", LatestProductsList.as_view()),
+    path("products/search/", search),
     path("products/<slug:category_slug>/<slug:product_slug>/", ProductDetail.as_view()),
+    path("products/<slug:category_slug>/", CategoryDetail.as_view()),
     path("products/<slug:category_slug>/", CategoryDetail.as_view())
 ]
