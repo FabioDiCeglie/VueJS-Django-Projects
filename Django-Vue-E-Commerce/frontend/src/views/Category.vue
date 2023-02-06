1ae77c068c8f5889a31fe10917a13eabdbe1a015
FabioDiCeglie <dcfabio96@gmail.com>
2023-02-06 21:12:09 +0100

    feat: convert product to a component

--- Django-Vue-E-Commerce/frontend/src/views/Category.vue
+++ Django-Vue-E-Commerce/frontend/src/views/Category.vue
@@ -5,36 +5,24 @@
         <h2 class="is-size-2 has-text-centered">{{ category.name }}</h2>
       </div>
 
-      <div
-        class="column is-3"
+      <ProductBox
         v-for="product in category.products"
         v-bind:key="product.id"
-      >
-        <div class="box">
-          <figure class="image mb-4">
-            <img :src="product.get_thumbnail" />
-          </figure>
-
-          <h3 class="is-size-4">{{ product.name }}</h3>
-          <p class="is-size-6 has-text-grey">${{ product.price }}</p>
-
-          <router-link
-            v-bind:to="product.get_absolute_url"
-            class="button is-dark mt-4"
-            >View details</router-link
-          >
-        </div>
-      </div>
+        v-bind:product="product"
+      />
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import { toast } from "bulma-toast";
+import ProductBox from "@/components/ProductBox.vue";
 
 export default {
   name: "Category",
+  components: {
+    ProductBox,
+  },
   data() {
     return {
       category: {
@@ -44,6 +32,13 @@ export default {
   },
   mounted() {
     this.getCategory();
+  },
+  watch: {
+    $route(to, from) {
+      if (to.name === "Category") {
+        this.getCategory();
+      }
+    },
   },
   methods: {
     async getCategory() {
