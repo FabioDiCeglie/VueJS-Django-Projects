b2121ae4af01de25ad19dfcb76d5003f4cbf903f
FabioDiCeglie <dcfabio96@gmail.com>
2023-02-06 21:02:36 +0100

    feat: make it possible to show a category

--- Django-Vue-E-Commerce/frontend/src/router/index.js
+++ Django-Vue-E-Commerce/frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "../views/Home.vue";
 import Product from "../views/Product.vue";
+import Category from "../views/Category.vue";
 
 const routes = [
   {
@@ -22,6 +23,11 @@ const routes = [
     path: "/:category_slug/:product_slug",
     name: "Product",
     component: Product,
+  },
+  {
+    path: "/:category_slug",
+    name: "Category",
+    component: Category,
   },
 ];
 
