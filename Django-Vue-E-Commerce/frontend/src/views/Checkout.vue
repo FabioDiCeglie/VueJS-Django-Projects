cc4e5933ed0568c09b0a850ddf7234605e460baf
FabioDiCeglie <dcfabio96@gmail.com>
2023-02-13 18:06:21 +0100

    feat: create a simple checkout page

--- Django-Vue-E-Commerce/frontend/src/views/Checkout.vue
+++ Django-Vue-E-Commerce/frontend/src/views/Checkout.vue
@@ -34,6 +34,83 @@
           </tfoot>
         </table>
       </div>
+
+      <div class="column is-12 box">
+        <h2 class="subtitle">Shipping details</h2>
+
+        <p class="has-text-grey mb-4">* All fields are required</p>
+
+        <div class="columns is-multiline">
+          <div class="column is-6">
+            <div class="field">
+              <label>First name*</label>
+              <div class="control">
+                <input type="text" class="input" v-model="first_name" />
+              </div>
+            </div>
+
+            <div class="field">
+              <label>Last name*</label>
+              <div class="control">
+                <input type="text" class="input" v-model="last_name" />
+              </div>
+            </div>
+
+            <div class="field">
+              <label>E-mail*</label>
+              <div class="control">
+                <input type="email" class="input" v-model="email" />
+              </div>
+            </div>
+
+            <div class="field">
+              <label>Phone*</label>
+              <div class="control">
+                <input type="text" class="input" v-model="phone" />
+              </div>
+            </div>
+          </div>
+
+          <div class="column is-6">
+            <div class="field">
+              <label>Address*</label>
+              <div class="control">
+                <input type="text" class="input" v-model="address" />
+              </div>
+            </div>
+
+            <div class="field">
+              <label>Zip code*</label>
+              <div class="control">
+                <input type="text" class="input" v-model="zipcode" />
+              </div>
+            </div>
+
+            <div class="field">
+              <label>Place*</label>
+              <div class="control">
+                <input type="text" class="input" v-model="place" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="notification is-danger mt-4" v-if="errors.length">
+          <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
+        </div>
+
+        <hr />
+
+        <div id="card-element" class="mb-5"></div>
+
+        <template v-if="cartTotalLength">
+          <hr />
+
+          <button class="button is-dark" @click="submitForm">
+            Pay with Stripe
+          </button>
+        </template>
+      </div>
     </div>
   </div>
 </template>
