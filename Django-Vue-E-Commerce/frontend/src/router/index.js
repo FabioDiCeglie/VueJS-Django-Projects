14ba765a6b285b16bf6f70136e12d28f1d4332cd
FabioDiCeglie <dcfabio96@gmail.com>
2023-02-13 18:08:34 +0100

    chore: created a simple success page

--- Django-Vue-E-Commerce/frontend/src/router/index.js
+++ Django-Vue-E-Commerce/frontend/src/router/index.js
@@ -10,6 +10,7 @@ import SignUp from "@/views/SignUp.vue";
 import LogIn from "@/views/LogIn.vue";
 import MyAccount from "@/views/MyAccount.vue";
 import Checkout from "@/views/Checkout.vue";
+import Success from "@/views/Success.vue";
 
 const routes = [
   {
@@ -53,6 +54,11 @@ const routes = [
     meta: {
       requireLogin: true,
     },
+  },
+  {
+    path: "/cart/success",
+    name: "Success",
+    component: Success,
   },
   {
     path: "/sign-up",
